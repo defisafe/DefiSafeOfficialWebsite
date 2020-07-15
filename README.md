058f0acc36af0ae1ac9e215239436e207d5b21d0
marui <761564424@qq.com>
2020-07-15 17:12:50 +0800

    Init website(#1) *init app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# DefiSafeOfficialWebsite
-DefiSafe official website page.
+# defisafe官网
+### defisafe是一个去中心化的投保安全协议，支持多种加密资产投保。
---
+++ build/config/pages.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const pages = [
+  {
+    name: 'index',
+    template: 'index/index.html',
+    entry: 'index/index.js',
+    title: '首页'
+  }
+]
+
+module.exports = pages;
